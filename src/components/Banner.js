c45798f6e6436cdfdaecc54dd3cb4bb747678f12
kwasianderson <kwasianderson01@gmail.com>
2022-12-15 15:36:40 +0000

    ok, deploiement

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,7 +16,7 @@ export const Banner = () => {
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Bienvenu sur mon Portofolio</span>
                   <h1>{`Je suis AndersonK, `}
-                    <span className="wrap">Deveppeur Web/Mobile</span>
+                    <span className="wrap">Deveppeur Web/Mobile, Administrateur bases de donnees mysql</span>
                   
                   </h1>
                   <p>J'ai des compétences dans plusieurs domaines
