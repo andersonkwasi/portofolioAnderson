146d7597dc175c64253d11a869becb26dd96ffaf
kwasianderson <kwasianderson01@gmail.com>
2022-12-15 15:32:11 +0000

    text typing delete

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import monImage from "../assets/img/monImage.png"
 import { ArrowRightCircle } from 'react-bootstrap-icons';
@@ -6,47 +5,7 @@ import TrackVisibility from 'react-on-screen';
 
 
 export const Banner = () => {
-  let wordIndex = 0
-  let letterIndex = 0
-  const [text, setText] = useState('');
-
-  const competence = [
-    "Data Base Administrator",
-    "Web/Mobile Developer",
-    "Linux Administrator",
-    "Analyst CyberSecurity"
-  ];
-  const createLetter = () => {
-    text.textContent = competence[wordIndex][letterIndex]
-    setTimeout(() => {
-      text.remove()
-    }, 2800)
-  }
-
-  const loop = () => {
-    setTimeout(() => {
-      if (wordIndex >= competence.length) {
-        wordIndex = 0
-        letterIndex = 0
-        loop()
-      }
-      else if (letterIndex < competence[wordIndex].length) {
-        createLetter()
-        letterIndex++
-        loop()
-      } else {
-        wordIndex++
-        letterIndex = 0
-        setTimeout(() => {
-          loop()
-        }, 2800)
-      }
-    }, 60)
-  }
-  loop()
-
-
-
+  
   return (
     <section className="banner" id="home">
       <Container>
@@ -56,9 +15,9 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Bienvenu sur mon Portofolio</span>
-                  <h1>{`Je suis AndersonK, `} <span className="txt-rotate">
-                    <span className="wrap">{text}</span>
-                  </span>
+                  <h1>{`Je suis AndersonK, `}
+                    <span className="wrap">Deveppeur Web/Mobile</span>
+                  
                   </h1>
                   <p>J'ai des compétences dans plusieurs domaines
                     Notemment dans l'Administration système Linux, Administration de bases de donnees,
